e752731a1ec356bb622434c1696d3906a0ae4475
tsejx <tsejx@foxmail.com>
2020-04-26 23:57:36 +0800

    :recycle: refactor: changed documentation tool from vuepress to dumi

---
+++ config/config.js
@@ -0,0 +1,20 @@
+export default {
+  mode: 'site',
+  title: 'Node Guidebook',
+  description: 'Node 完全知识体系',
+  base: '/node-guidebook/',
+  publicPath: '/node-guidebook/',
+  favicon: './favicon.ico',
+  logo: './favicon.svg',
+  exportStatic: {},
+  theme: {
+    '@primary-color': '#3d8e96',
+  },
+  navs: [
+    null,
+    {
+      title: 'Github',
+      path: 'https://github.com/tsejx/node-guidebook',
+    },
+  ],
+};
